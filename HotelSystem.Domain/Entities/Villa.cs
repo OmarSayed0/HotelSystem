254aa62b677624a19b98b8ec36424cc5d1b0f0aa
OmarSayed0 <omarsayed.cpp@gmail.com>
2024-10-16 20:57:57 +0300

    Villa Model and CRUD

--- HotelSystem.Domain/Entities/Villa.cs
+++ HotelSystem.Domain/Entities/Villa.cs
@@ -11,11 +11,16 @@ namespace HotelSystem.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set  ; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
+        [Range(10, 10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1, 10)]
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
         public DateTime? Updated_Date { get; set; }
