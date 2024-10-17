85203bef24f62d9e9e7163b956aeaed07c0d9fe3
OmarSayed0 <omarsayed.cpp@gmail.com>
2024-10-17 11:25:31 +0300

    Add Notification on CRUD

--- HotelSystem.Web/Controllers/VillaController.cs
+++ HotelSystem.Web/Controllers/VillaController.cs
@@ -32,6 +32,7 @@ namespace HotelSystem.Web.Controllers
             {
                 _db.Villas.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully.";
                 return RedirectToAction(nameof(Index));
             }
             return View();
@@ -54,6 +55,8 @@ namespace HotelSystem.Web.Controllers
             {
                 _db.Villas.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
+
                 return RedirectToAction(nameof(Index));
             }
             return View();
@@ -77,8 +80,10 @@ namespace HotelSystem.Web.Controllers
             {
                 _db.Villas.Remove(objFromDb);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
                 return RedirectToAction(nameof(Index));
             }
+            TempData["error"] = "The villa could not be deleted.";
             return View();
         }
 
