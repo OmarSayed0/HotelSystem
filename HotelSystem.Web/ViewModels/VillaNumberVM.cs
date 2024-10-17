2527fb87be5a1483021c10e82ac20f024e4f13d0
OmarSayed0 <omarsayed.cpp@gmail.com>
2024-10-17 15:22:42 +0300

    Villa Number and CRUD

---
+++ HotelSystem.Web/ViewModels/VillaNumberVM.cs
@@ -0,0 +1,13 @@
+﻿using HotelSystem.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HotelSystem.Web.ViewModels
+{
+    public class VillaNumberVM
+    {
+        public VillaNumber? VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? VillaList { get; set; }
+    }
+}
